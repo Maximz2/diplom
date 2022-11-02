956d64c68bbccc801895613478989172d352963f
Maxim <99118327+Maximz2@users.noreply.github.com>
2022-11-02 12:13:00 +0300

    echo bot

--- src/todolist/settings.py
+++ src/todolist/settings.py
@@ -200,6 +200,8 @@ SOCIAL_AUTH_VK_EXTRA_DATA = [
 SOCIAL_AUTH_NEW_USER_REDIRECT_URL = '/logged-in/'
 SOCIAL_AUTH_USER_MODEL = 'core.User'
 
+BOT_TOKEN = env.str('BOT_TOKEN', default='')
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_FILTER_BACKENDS': (
